363e9a200ad2dd96f71f0478e75136029c655642
xavier <xmb5563@rit.edu>
2019-12-17 16:26:12 -0500

    includes a newer feed line subroutine

--- functions.asm
+++ functions.asm
@@ -47,3 +47,18 @@ quit:
     mov     eax, 1
     int     80h
     ret
+
+;------------------------------------------
+; void sprintLF(String message)
+; String printing with line feed function
+sprintLF:
+    call    sprint
+ 
+    push    eax         ; push eax onto the stack to preserve it while we use the eax register in this function
+    mov     eax, 0Ah    ; move 0Ah into eax - 0Ah is the ascii character for a linefeed
+    push    eax         ; push the linefeed onto the stack so we can get the address
+    mov     eax, esp    ; move the address of the current stack pointer into eax for sprint
+    call    sprint      ; call our sprint function
+    pop     eax         ; remove our linefeed character from the stack
+    pop     eax         ; restore the original value of eax before our function was called
+    ret                 ; return to our program
